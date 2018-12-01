f16b2c0b73d5cd066f76b71248c9dc045b891057
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-12-01 21:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,27 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100);
+ var item_object = {};
+ item_object[item] = price;
+ cart.push(item_object)
+ console.log(`${item} has been added to your cart.`)
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  } else {
+    var items = [];
+    for(let i = 0;i < cart.length;i++){
+      for(var item in cart[i]) {
+        items.push(item + "at $" + cart[i][item])
+      }
+    }
+    return "In your cart, you have " + items.join(", ") + ".";
+  }
 }
 
 function total() {
